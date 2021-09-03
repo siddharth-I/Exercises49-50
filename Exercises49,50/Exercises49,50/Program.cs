24defc5056cce94fea0daff03796f5fe938af194
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2021-09-03 10:03:47 -0400

    Add files via upload

---
+++ Exercises49,50/Exercises49,50/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercises49_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a side length:");
+            int length1 = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Exercises49,50/Exercises49,50/Triangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises49_50
+{
+    class Triangle
+    {
+        public int Length;
+    }
+}
